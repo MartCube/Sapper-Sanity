2e6a6bf62783db73f68c7689e59267c70a678951
mariiao <marya.vadymovna@gmail.com>
2023-02-08 22:30:46 +0100

    articleContent updated

--- schemas/content/articleContent.ts
+++ schemas/content/articleContent.ts
@@ -12,6 +12,10 @@ export default defineType({
 			title: 'Richtext',
 			type: 'richtext',
 		}),
+		defineArrayMember({
+			title: 'Richtext Image',
+			type: 'richtextImage',
+		}),
 		// You can add additional types here. Note that you can't use
 		// primitive types such as 'string' and 'number' in the same array
 		// as a block type.
